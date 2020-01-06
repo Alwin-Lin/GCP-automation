98ad17c50b8c30d376a8e913820806d142ab14c0
Alwin-Lin <alwin001@gmail.com>
2020-01-05 17:52:01 -0800

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -7,11 +7,3 @@ cd ../android
 gcloud builds submit --config=cloudbuild-ndk.yaml --substitutions=_ANDROID_VERSION=29
 gsutil mb gs://apk-ci-gcpapk
 gsutil mb gs://apk-ci-gcptemp
-
-
-cd ~/cloudBuildWS
-git clone https://github.com/Alwin-Lin/performance-samples.git
-cd ../GCP-automation/Template
-cp cloudbuild.yaml /home/alwin001/cloudBuildWS/performance-samples/BenchmarkSample/
-cd ~/cloudBuildWS/performance-samples/BenchmarkSample/
-cloud-build-local --dryrun=false --substitutions=_CACHE_BUCKET=apk-ci-gcptemp,_ARTIFACT_BUCKET=apk-ci-gcpapk cloudbuild.yaml
