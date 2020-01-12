35b4f7a95f1a53cd3cf8ee35f1be4b2efba08012
Alwin-Lin <alwin001@gmail.com>
2020-01-12 13:54:49 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # A simple script to setup container images and cloud storage buckets
 echo "Clones cloud builder into file cloudBuildWS"
 mkdir ~/cloudBuildWS;cd ~/cloudBuildWS
+# https://github.com/GoogleCloudPlatform/cloud-builders-community.git
 git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
 cd cloud-builders-community
 
@@ -9,11 +10,25 @@ echo "Building tar and Android SDK"
 cd tar
 gcloud builds submit --config=cloudbuild.yaml
 cd ../android
-echo "ndk or sdk?"
-read version
-echo "Chose Android Version"
-read number
-gcloud builds submit --config=cloudbuild-$version.yaml --substitutions=_ANDROID_VERSION=$number
+if [[ -z "${ANDROID_SDK_IMAGE_TYPE}" ]]; then
+  ANDROID_SDK_IMAGE_TYPE="SDK"
+fi
+echo "Android image type is $ANDROID_SDK_IMAGE_TYPE"
+if [[ -z "${ANDROID_SDK_API_LEVEL}" ]]; then
+  ANDROID_SDK_API_LEVEL=28
+fi
+echo "Android image type is $ANDROID_SDK_API_LEVEL"
+
+if [ $ANDROID_SDK_IMAGE_TYPE = "SDK" ]
+then gcloud builds submit --config=cloudbuild.yaml
+else gcloud builds submit --config=cloudbuild-ndk.yaml
+fi
+
+if [ ANDROID_SDK_API_LEVEL=28 ]
+then gcloud builds submit --substitutions=_ANDROID_VERSION=28
+else gcloud builds submit --substitutions=_ANDROID_VERSION=$ANDROID_SDK_API_LEVEL
+fi
+
 
 echo "Setting up cloud storage for cache and apk"
 echo "Name your artifact bucket"
