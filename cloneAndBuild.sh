a74f57a81e7cc30f594be92214224dfcbb4df803
Alwin-Lin <alwin001@gmail.com>
2020-01-12 14:52:56 -0800

    Update cloneAndBuild.sh

--- cloneAndBuild.sh
+++ cloneAndBuild.sh
@@ -6,10 +6,9 @@ git clone $link
 
 echo "adding cloud config to project"
 cd ../GCP-automation/Template
-echo "Paste path to project"
-read $pathToProject
-cp cloudbuild.yaml $pathToProject
+$PATH_TO_PROJECT= "~/cloudBuildWS/$link
+cp cloudbuild.yaml $PATH_TO_PROJECT
 
 echo "Starting test build"
-cd ~$pathToProject
-cloud-build-local --dryrun=false --substitutions=_CACHE_BUCKET=$cache,_ARTIFACT_BUCKET=$apk cloudbuild.yaml
+cd ~$PATH_TO_PROJECT
+cloud-build-local --dryrun=false --substitutions=_CACHE_BUCKET=$CACHE_BUCKET_NAME,_ARTIFACT_BUCKET=$APK_BUCKET_NAME cloudbuild.yaml
