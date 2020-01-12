e303814ce303c93d9506912af431bf9030562aa7
Alwin-Lin <alwin001@gmail.com>
2020-01-12 15:29:30 -0800

    Update cloneAndBuild.sh

--- cloneAndBuild.sh
+++ cloneAndBuild.sh
@@ -1,18 +1,20 @@
 echo "Cloning GitHub project"
 cd ~/cloudBuildWS
 echo "Paste your github project link"
-read $url
-git clone $url
+read $git_URL
+git clone $git_URL
 
 echo "adding cloud config to project"
 cd ../GCP-automation/Template
 
 echo "getting file location"
-wholeFile = ${url##*/}
-fileName = ${wholeFile%.*}
-$PATH_TO_PROJECT= "~/cloudBuildWS/$fileName
+gitProjectName = ${git_URL##*/}
+projectName = ${gitProjectName%.*}
+$PATH_TO_PROJECT= "~/cloudBuildWS/$projectName
+
+echo "adding cloudbuild.yaml"
 cp cloudbuild.yaml $PATH_TO_PROJECT
 
-echo "Starting test build"
+echo "Starting local test run"
 cd ~$PATH_TO_PROJECT
 cloud-build-local --dryrun=false --substitutions=_CACHE_BUCKET=$CACHE_BUCKET_NAME,_ARTIFACT_BUCKET=$APK_BUCKET_NAME cloudbuild.yaml
