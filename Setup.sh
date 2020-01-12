ed644f944ddfbbdc86f3d3eed64d71caee477adc
Alwin-Lin <alwin001@gmail.com>
2020-01-12 10:58:28 -0800

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+# A simple script to setup container images and cloud storage buckets
+echo "Clones cloud builder into file cloudBuildWS"
 mkdir ~/cloudBuildWS;cd ~/cloudBuildWS
 git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
 cd cloud-builders-community
+
+echo "Building tar and Android SDK"
 cd tar
 gcloud builds submit --config=cloudbuild.yaml
 cd ../android
@@ -9,6 +14,8 @@ read version
 echo "Chose Android Version"
 read number
 gcloud builds submit --config=cloudbuild-$version.yaml --substitutions=_ANDROID_VERSION=$number
+
+echo "Setting up cloud storage for cache and apk"
 echo "Name your artifact bucket"
 read apk
 gsutil mb gs://$apk
