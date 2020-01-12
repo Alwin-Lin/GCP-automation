0c5e5fcbd2ebe90c2e920fc50f94f8b8eca66886
Alwin-Lin <alwin001@gmail.com>
2020-01-12 11:45:54 -0800

    Create triggerCreation.sh

---
+++ triggerCreation.sh
@@ -0,0 +1,11 @@
+echo "Creating trigger for GitHub"
+echo "All hail our Robot overloards"
+gcloud beta builds triggers create github \
+echo "Input name of github reposetory"
+read $REPO_NAME
+--repo-name=$REPO_NAME\
+echo "Input name of github reposetory"
+read $REPO_OWNER
+--repo-owner=$REPO_OWNER\
+--branch-pattern=".*" \
+--build-config=$pathToProject \
