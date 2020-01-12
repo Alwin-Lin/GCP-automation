3ecfda8cbfe8e34a672ed58fa00f113eb861ccc1
Alwin-Lin <alwin001@gmail.com>
2020-01-12 14:27:25 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,14 +27,14 @@ fi
 echo "Setting up cloud storage for cache and apk"
 
 if [[ -z "$APK_BUCKET_NAME" ]]; then
- APK_BUCKET_NAME="$HOSTNAME_apk"
+ APK_BUCKET_NAME=$HOSTNAME_apk
 fi
 echo "Creating bucket for APK: $APK_BUCKET_NAME"
 
 if [[ -z "$CACHE_BUCKET_NAME" ]]; then
- CACHE_BUCKET_NAME="$HOSTNAME_cache"
+ CACHE_BUCKET_NAME=$HOSTNAME_cache
 fi
 echo "Creating bucket for cache : $CACHE_BUCKET_NAME"
 
-gsutil mb -p gs://$APK_BUCKET_NAME
-gsutil mb -p gs://$CACHE_BUCKET_NAME
+gsutil mb gs://$APK_BUCKET_NAME
+gsutil mb gs://$CACHE_BUCKET_NAME
