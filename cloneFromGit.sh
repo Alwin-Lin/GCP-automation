b51fa5c86b3953de98e775162fbc8cae5df6c03b
Alwin-Lin <alwin001@gmail.com>
2020-01-12 10:56:48 -0800

    Update cloneFromGit.sh

--- cloneFromGit.sh
+++ cloneFromGit.sh
@@ -1,10 +1,13 @@
+echo "Cloning GitHub project"
 cd ~/cloudBuildWS
 echo "Paste your github project link"
 read $link
 git clone $link
+echo "adding cloud config to project"
 cd ../GCP-automation/Template
 echo "Paste path to project"
 read $pathToProject
 cp cloudbuild.yaml $pathToProject
+echo "Starting test build"
 cd ~$pathToProject
 cloud-build-local --dryrun=false --substitutions=_CACHE_BUCKET=$cache,_ARTIFACT_BUCKET=$apk cloudbuild.yaml
