e545eb0e6462fe46e35d5ce1473274f1b414827e
Alwin-Lin <alwin001@gmail.com>
2020-01-12 16:13:11 -0800

    Update triggerCreation.sh

--- triggerCreation.sh
+++ triggerCreation.sh
@@ -1,10 +1,22 @@
-echo "Creating trigger for GitHub"
+# Creates a trigger conecting google cloud and github
 gcloud beta builds triggers create github \
-echo "Input name of github reposetory"
-read $REPO_NAME
---repo-name=$REPO_NAME\
-echo "Input name of github reposetory"
-read $REPO_OWNER
---repo-owner=$REPO_OWNER\
+
+if [[ -z "${APP_GITHUB_URL}" ]]; then
+  APP_GITHUB_URL="https://github.com/googlecodelabs/android-testing.git"
+fi
+echo "Curent project is $APP_GITHUB_URL"
+
+NAME_AND_OWNER_FILE=${APP_GITHUB_URL#*/}
+echo "Getting project name"
+echo "Getting project owner"
+
+NAME_FILE=${NAME_AND_OWNER#*/}
+NAME=${NAME_FILE%.*}
+OWNER=${NAME_AND_OWNER##*/}
+
+--repo-name=[$NAME] \
+echo "reposetory named as $NAME"
+--repo-owner=[$OWNER] \
+echo "The owner of this is $OWNER"
 --branch-pattern=".*" \
---build-config=$pathToProject \
+--build-config=[~/GCP-automation/Template/cloudbuild.yaml] \
